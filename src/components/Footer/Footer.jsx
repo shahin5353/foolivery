014cff6f55d293bb7397a142207222b762102c5d
Shahin <ahmshahin5353@gmail.com>
2020-09-07 16:54:48 +0600

    footer added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,10 +3,11 @@ import './Footer.scss';
 import logo from '../../assets/foolivery-logo.png'
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faPhone, faPhoneAlt, faClock, faMailBulk, faEnvelope} from '@fortawesome/free-solid-svg-icons'
 const Footer = () => {
     return (
-      <>
-      <hr className="bg-success" style={{height:'20px', color:'#'}}/>
+      <div className="container">
+      <div className="my-4" id="top-hr"/>
         <nav
         className="navbar navbar-expand"
         id="main-nav"
@@ -38,12 +39,37 @@ const Footer = () => {
       <hr className="my-5"/>
       <div className="row">
         <div className="col-5">
-
+          <h5 className="mb-4">Sign up for Updates</h5>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione atque molestiae provident? Consectetur, dolorum!</p>
+          <input className="form-control" type="text" name="" id="" placeholder="Enter your mail"/>
+          <p className="mt-3">&copy; 2020 Food Delivery Template-Foolivery by Shahin</p>
         </div>
-        <div className="col-3"></div>
-        <div className="col-4"></div>
+        <div className="col-3">
+          <div className="row">
+          <h5 className="mb-4">Sign up for Updates</h5>
+            <div className="col-6">
+              <li className="text-success">Main</li>
+              <li className="text-success">About us</li>
+              <li className="text-success">News</li>
+              <li className="text-success">Menu</li>
+              <li className="text-success">FAQ</li>
+            </div>
+            <div className="col-6">
+              <li className="text-success">Blog</li>
+              <li className="text-success">Contacts</li>
+              <li className="text-success">Privacy</li>
+              <li className="text-success">Terms</li>
+            </div>
+          </div>
+        </div>
+        <div className="col-4">
+        <h5 className="mb-4">Contact us</h5>
+        <li className="list-unstyled mb-2"><FontAwesomeIcon className="text-success" icon={faPhoneAlt}/> 8 (800) 000 00 00</li>
+        <li className="list-unstyled mb-2"><FontAwesomeIcon className="text-success" icon={faClock}/> Monday-Friday: 9:00 am - 6:00 pm Saturday: 9:00 am - 4:00pm</li>
+        <li className="list-unstyled"><FontAwesomeIcon className="text-success" icon={faEnvelope}/> hello@shahin.developer</li>
+        </div>
       </div>
-      </>
+      </div>
     );
 };
 
